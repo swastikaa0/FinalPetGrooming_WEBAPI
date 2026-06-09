0d9eb47e84ddaaf2ce6ab0bd3e157291ee03b4a6
swastikaa0 <swastikadhungana2004@gmail.com>
2026-06-09 16:46:44 +0545

    Axios-instance

---
+++ lib/api/axios-instance.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASIC
+    || "http://localhost:5000";
+
+const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+export default axiosInstance;
