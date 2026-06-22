cbb0c7c1f95887e24091e0ad9c209b5229c5ee6f
swastikaa0 <swastikadhungana2004@gmail.com>
2026-06-22 13:52:40 +0545

    auth

--- lib/api/axios-instance.ts
+++ lib/api/axios-instance.ts
@@ -1,13 +1,27 @@
 import axios from "axios";
+import { getTokenCookie } from "../cookies";
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_BASIC
     || "http://localhost:5000";
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
-    headers: {
-        "Content-Type": "application/json",
-    },
+   
 });
+axiosInstance.interceptors.request.use(
+    async (config) => {
+        const token = await getTokenCookie();
+        if (token) {
+            config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        if (!(config.data instanceof FormData) && !config.headers["Content-Type"]) {
+            config.headers["Content-Type"] = "application/json";
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default axiosInstance;
