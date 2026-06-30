b5e37b131cefcf6fd5c6d9b6ed01bfdc87e18984
swastikaa0 <swastikadhungana2004@gmail.com>
2026-06-30 16:59:14 +0545

    admin endpoints added

--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -5,5 +5,15 @@ export const API ={
         WHOAMI:"/api/v1/auth/whoami",
         UPDATE:'/api/v1/auth/update',
          UPDATE_PASSWORD: "/api/v1/auth/update-password"
+    },
+     ADMIN: {
+        USERS: {
+            GET_ALL: "/api/v1/admin/users",
+            GET_BY_ID: (id: string) => `/api/v1/admin/users/${id}`,
+            CREATE: "/api/v1/admin/users",
+            UPDATE: (id: string) => `/api/v1/admin/users/${id}`,
+            UPDATE_PASSWORD: (id: string) => `/api/v1/admin/users/${id}/password`,
+            DELETE: (id: string) => `/api/v1/admin/users/${id}`,
+        }
     }
 }
