29160dbece29ce37fcae0e8deda886e360c20494
swastikaa0 <swastikadhungana2004@gmail.com>
2026-07-28 20:52:19 +0545

    endpoints api changes

--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -4,7 +4,9 @@ export const API ={
         LOGIN:"/api/v1/auth/login",
         WHOAMI:"/api/v1/auth/whoami",
         UPDATE:'/api/v1/auth/update',
-         UPDATE_PASSWORD: "/api/v1/auth/update-password"
+         UPDATE_PASSWORD: "/api/v1/auth/update-password",
+         FORGOT_PASSWORD: "/api/v1/auth/forgot-password",
+    RESET_PASSWORD: "/api/v1/auth/reset-password",
     },
      ADMIN: {
         USERS: {
@@ -14,6 +16,59 @@ export const API ={
             UPDATE: (id: string) => `/api/v1/admin/users/${id}`,
             UPDATE_PASSWORD: (id: string) => `/api/v1/admin/users/${id}/password`,
             DELETE: (id: string) => `/api/v1/admin/users/${id}`,
-        }
-    }
-}
+        },
+         SERVICES: {
+            GET_ALL: "/api/v1/admin/services",
+            CREATE: "/api/v1/admin/services",
+            GET_BY_ID: (id: string) => `/api/v1/admin/services/${id}`,
+            UPDATE: (id: string) => `/api/v1/admin/services/${id}`,
+            DELETE: (id: string) => `/api/v1/admin/services/${id}`,
+  },
+    },
+    PET: {
+    CREATE: "/api/v1/pets",
+    GET_ALL: "/api/v1/pets",
+    UPDATE: (id: string) => `/api/v1/pets/${id}`,
+    DELETE: (id: string) => `/api/v1/pets/${id}`,
+  },
+
+  BOOKING: {
+    CREATE: "/api/v1/bookings",
+
+    GET_ALL: "/api/v1/bookings",
+
+     // admin all bookings
+  GET_ALL_BOOKINGS:"api/v1/bookings/all",
+
+
+    GET_BY_ID: (id: string) =>
+      `/api/v1/bookings/${id}`,
+
+    UPDATE: (id: string) =>
+      `/api/v1/bookings/${id}`,
+
+    CANCEL: (id: string) =>
+      `/api/v1/bookings/${id}/cancel`,
+
+    DELETE: (id: string) =>
+      `/api/v1/bookings/${id}`,
+  },
+
+  SERVICE: {
+  GET_ACTIVE: "/api/v1/services/active",
+},
+
+NOTIFICATION: {
+    GET_ALL: "/api/v1/notifications",
+    MARK_ALL_READ: "/api/v1/notifications/read-all",
+    DELETE_ALL: "/api/v1/notifications/delete-all",
+
+    MARK_AS_READ: (id:string)=>
+        `/api/v1/notifications/${id}/read`,
+
+    DELETE:(id:string)=>
+        `/api/v1/notifications/${id}`,
+},
+
+  
+};
