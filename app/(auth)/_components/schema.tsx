cfdac9ede18ca881b3faac679f60e68c720f358d
swastikaa0 <swastikadhungana2004@gmail.com>
2026-07-28 20:48:58 +0545

    changes

--- app/(auth)/_components/schema.tsx
+++ app/(auth)/_components/schema.tsx
@@ -24,3 +24,29 @@ export const loginSchema = z.object({
 });
 
 export type LoginFormData = z.infer<typeof loginSchema>;
+
+export const forgotPasswordSchema = z.object({
+    email: z.email("Invalid email address"),
+});
+
+export type ForgotPasswordFormData = z.infer<
+    typeof forgotPasswordSchema
+>;
+
+export const resetPasswordSchema = z.object({
+    password: z.string()
+        .min(6, "Password must be at least 6 characters long"),
+
+    confirmPassword: z.string()
+        .min(6, "Confirm Password must be at least 6 characters long"),
+}).refine(
+    (data) => data.password === data.confirmPassword,
+    {
+        message: "Passwords do not match",
+        path: ["confirmPassword"],
+    }
+);
+
+export type ResetPasswordFormData = z.infer<
+    typeof resetPasswordSchema
+>;
